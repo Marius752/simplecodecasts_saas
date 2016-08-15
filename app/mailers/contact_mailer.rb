c68790719bb376ef858ca5b4cb071dba52f48689
Marius Kuzminskas <marius.code.all@gmail.com>
2016-08-15 17:39:01 +0000

    Trying to find emlail problem edited cotact mailer.rb and contact emai.html.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,12 @@
+###app/mailers/contact_mailer.rb
 class ContactMailer < ActionMailer::Base
-    default to: 'marius.code.all@gmail.com'
+  default to: 'your@email_address.com'
+  
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
     
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        
-        mail(from: email, subject: 'Contact Form Message')
-    end
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
